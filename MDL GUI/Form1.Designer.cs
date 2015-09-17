40991bf8e357ded1f75ee9a5e44fa393701cc0d6
Pete Goodfellow <petethegoat@gmail.com>
2015-09-16 20:37:20 -0500

    Adding initial files.

---
+++ MDL GUI/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MDL_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonBrowseTarget = new System.Windows.Forms.Button();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.buttonCompile = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.decompMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.buttonGotoTarget = new System.Windows.Forms.Button();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.byPetethegoatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forkMeOnGithubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.buttonBrowseTarget);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxTarget);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonCompile);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonGotoTarget);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxConsole);
+            this.splitContainer1.Size = new System.Drawing.Size(464, 282);
+            this.splitContainer1.SplitterDistance = 88;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonBrowseTarget
+            // 
+            this.buttonBrowseTarget.Location = new System.Drawing.Point(323, 27);
+            this.buttonBrowseTarget.Name = "buttonBrowseTarget";
+            this.buttonBrowseTarget.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowseTarget.TabIndex = 5;
+            this.buttonBrowseTarget.Text = "Browse";
+            this.buttonBrowseTarget.UseVisualStyleBackColor = true;
+            this.buttonBrowseTarget.Click += new System.EventHandler(this.buttonBrowseTarget_Click);
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.Location = new System.Drawing.Point(11, 29);
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.ReadOnly = true;
+            this.textBoxTarget.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxTarget.Size = new System.Drawing.Size(306, 20);
+            this.textBoxTarget.TabIndex = 3;
+            // 
+            // buttonCompile
+            // 
+            this.buttonCompile.Location = new System.Drawing.Point(11, 56);
+            this.buttonCompile.Name = "buttonCompile";
+            this.buttonCompile.Size = new System.Drawing.Size(425, 23);
+            this.buttonCompile.TabIndex = 0;
+            this.buttonCompile.Text = "Compile";
+            this.buttonCompile.UseVisualStyleBackColor = true;
+            this.buttonCompile.Click += new System.EventHandler(this.buttonCompile_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compMenu,
+            this.decompMenu});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // compMenu
+            // 
+            this.compMenu.Name = "compMenu";
+            this.compMenu.Size = new System.Drawing.Size(163, 22);
+            this.compMenu.Text = "MDL Compiler";
+            this.compMenu.Click += new System.EventHandler(this.compMenu_Click);
+            // 
+            // decompMenu
+            // 
+            this.decompMenu.Name = "decompMenu";
+            this.decompMenu.Size = new System.Drawing.Size(163, 22);
+            this.decompMenu.Text = "MDL Decompiler";
+            this.decompMenu.Click += new System.EventHandler(this.decompMenu_Click);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxConsole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxConsole.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBoxConsole.Location = new System.Drawing.Point(0, 0);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxConsole.Size = new System.Drawing.Size(462, 188);
+            this.textBoxConsole.TabIndex = 0;
+            // 
+            // buttonGotoTarget
+            // 
+            this.buttonGotoTarget.Location = new System.Drawing.Point(404, 27);
+            this.buttonGotoTarget.Name = "buttonGotoTarget";
+            this.buttonGotoTarget.Size = new System.Drawing.Size(32, 23);
+            this.buttonGotoTarget.TabIndex = 6;
+            this.buttonGotoTarget.Text = "..";
+            this.buttonGotoTarget.UseVisualStyleBackColor = true;
+            this.buttonGotoTarget.Click += new System.EventHandler(this.buttonGotoTarget_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.byPetethegoatToolStripMenuItem,
+            this.forkMeOnGithubToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // byPetethegoatToolStripMenuItem
+            // 
+            this.byPetethegoatToolStripMenuItem.Name = "byPetethegoatToolStripMenuItem";
+            this.byPetethegoatToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.byPetethegoatToolStripMenuItem.Text = "By Petethegoat";
+            this.byPetethegoatToolStripMenuItem.Click += new System.EventHandler(this.byPetethegoatToolStripMenuItem_Click);
+            // 
+            // forkMeOnGithubToolStripMenuItem
+            // 
+            this.forkMeOnGithubToolStripMenuItem.Name = "forkMeOnGithubToolStripMenuItem";
+            this.forkMeOnGithubToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.forkMeOnGithubToolStripMenuItem.Text = "Fork me on Github!";
+            this.forkMeOnGithubToolStripMenuItem.Click += new System.EventHandler(this.forkMeOnGithubToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 282);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "MDL GUI";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.Button buttonCompile;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compMenu;
+        private System.Windows.Forms.ToolStripMenuItem decompMenu;
+        private System.Windows.Forms.TextBox textBoxTarget;
+        private System.Windows.Forms.Button buttonBrowseTarget;
+        private System.Windows.Forms.Button buttonGotoTarget;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem byPetethegoatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forkMeOnGithubToolStripMenuItem;
+    }
+}
---
+++ MDL GUI/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+namespace MDL_GUI
+{
+    public partial class Form1 : Form
+    {
+        string mdlCompilerExe = null;
+        string mdlCompilerDir = null;
+
+        string mdlDecompilerExe = null;
+        string mdlDecompilerDir = null;
+
+        string filePath = null;
+        string fileDir = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadSettings();
+            UpdateInterface();
+        }
+
+        private void ReadSettings() //lol
+        {
+            if(File.Exists("settings.ini"))
+            {
+                string[] lines = File.ReadAllLines("settings.ini");
+                foreach(string line in lines)
+                {
+                    char[] c = new char[]{':'};
+                    string[] parts = line.Split(c, 2);
+                    switch(parts[0])
+                    {
+                        case "compilerDir":
+                            mdlCompilerDir = parts[1];
+                            break;
+                        case "compilerExe":
+                            mdlCompilerExe = parts[1];
+                            break;
+                        case "decompilerDir":
+                            mdlDecompilerDir = parts[1];
+                            break;
+                        case "decompilerExe":
+                            mdlDecompilerExe = parts[1];
+                            break;
+                        case "fileDir":
+                            fileDir = parts[1];
+                            break;
+                        case "filePath":
+                            filePath = parts[1];
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void WriteSettings() //lol
+        {
+            string[] lines = new string[6];
+            lines[0] = "compilerDir:" + mdlCompilerDir;
+            lines[1] = "compilerExe:" + mdlCompilerExe;
+            lines[2] = "decompilerDir:" + mdlDecompilerDir;
+            lines[3] = "decompilerExe:" + mdlDecompilerExe;
+            lines[4] = "fileDir:" + fileDir;
+            lines[5] = "filePath:" + filePath;
+            File.WriteAllLines("settings.ini", lines);
+        }
+
+        private void UpdateInterface()
+        {
+            decompMenu.Image = null;
+            if(mdlDecompilerExe != null)
+            {
+                decompMenu.ToolTipText = mdlDecompilerDir + "\\" + mdlDecompilerExe;
+                decompMenu.Image = Properties.Resources.tick;
+            }
+
+            compMenu.Image = null;
+            if(mdlCompilerExe != null)
+            {
+                compMenu.ToolTipText = mdlCompilerDir + "\\" + mdlCompilerExe;
+                compMenu.Image = Properties.Resources.tick;
+            }
+
+            if(filePath != null && filePath != "\\")
+            {
+                textBoxTarget.Text = fileDir + "\\" + filePath;
+                switch(Path.GetExtension(filePath))
+                {
+                    case ".qc":
+                        {
+                            buttonCompile.Text = "Compile";
+                            buttonCompile.Enabled = true;
+                            break;
+                        }
+                    case ".mdl":
+                        {
+                            buttonCompile.Text = "Decompile";
+                            buttonCompile.Enabled = true;
+                            break;
+                        }
+                    default:
+                        {
+                            buttonCompile.Text = "Unrecognized Extension";
+                            buttonCompile.Enabled = false;
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                buttonCompile.Text = "Compile";
+                buttonCompile.Enabled = false;
+            }
+            WriteSettings();
+        }
+
+        private void buttonCompile_Click(object sender, EventArgs e)
+        {
+            Directory.SetCurrentDirectory(fileDir);
+            MessageBox.Show(Directory.GetCurrentDirectory());
+            switch(Path.GetExtension(filePath))
+            {
+                case ".qc":
+                    {
+                        CMD(mdlCompilerDir + "\\" + mdlCompilerExe, fileDir + "\\" + filePath);
+                        break;
+                    }
+                case ".mdl":
+                    {
+                        CMD(mdlDecompilerDir + "\\" + mdlDecompilerExe, fileDir + "\\" + filePath);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if(files.Length > 1)
+            {
+                return;
+            }
+            filePath = Path.GetFileName(files[0]);
+            fileDir = Path.GetDirectoryName(files[0]);
+            UpdateInterface();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void decompMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select MDL Decompiler";
+            dialog.FileName = mdlDecompilerExe;
+            dialog.InitialDirectory = mdlDecompilerDir;
+            DialogResult result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                mdlDecompilerExe = dialog.SafeFileName;
+                mdlDecompilerDir = Path.GetDirectoryName(dialog.FileName);
+                UpdateInterface();
+            }
+        }
+
+        private void compMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select MDL Compiler";
+            dialog.FileName = mdlCompilerExe;
+            dialog.InitialDirectory = mdlCompilerDir;
+            DialogResult result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                mdlCompilerExe = dialog.SafeFileName;
+                mdlCompilerDir = Path.GetDirectoryName(dialog.FileName);
+                UpdateInterface();
+            }
+        }
+
+        private void buttonBrowseTarget_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select Target File";
+            dialog.FileName = filePath;
+            dialog.InitialDirectory = fileDir;
+            DialogResult result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                filePath = dialog.SafeFileName;
+                fileDir = Path.GetDirectoryName(dialog.FileName);
+                UpdateInterface();
+            }
+        }
+
+        private void CMD(string prog, string arguments)
+        {
+            var processStartInfo = new ProcessStartInfo(prog);
+
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.ErrorDialog = false;
+            processStartInfo.Arguments = arguments;
+
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = false;
+
+            Process process = new Process();
+            process.StartInfo = processStartInfo;
+            bool processStarted = process.Start();
+
+            StreamWriter inputWriter = process.StandardInput;
+            StreamReader outputReader = process.StandardOutput;
+            StreamReader errorReader = process.StandardError;
+            process.WaitForExit();
+
+            textBoxConsole.Text = outputReader.ReadToEnd();
+        }
+
+        private void buttonGotoTarget_Click(object sender, EventArgs e)
+        {
+            Process.Start(@fileDir);
+        }
+
+        private void byPetethegoatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/petethegoat/");
+        }
+
+        private void forkMeOnGithubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/petethegoat/");
+        }
+    }
+}
